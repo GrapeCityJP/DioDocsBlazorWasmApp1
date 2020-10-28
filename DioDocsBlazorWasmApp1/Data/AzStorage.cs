40845338841d02c7763038fd03fcebe446abbe9c
Kuni <kuni.ujiie@grapecity.com>
2020-10-28 11:32:42 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsBlazorWasmApp1/Data/AzStorage.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs;
+using System.IO;
+
+namespace DioDocsBlazorWasmApp1.Data
+{
+    public class AzStorage
+    {
+        private readonly string storageConnectionString;
+
+        public AzStorage(string connectionstring)
+        {
+            storageConnectionString = connectionstring;
+        }
+
+        public async void UploadExcelAsync(MemoryStream uploadstream)
+        {
+            BlobContainerClient container = new BlobContainerClient(storageConnectionString, "diodocs");
+
+            BlobClient blob = container.GetBlobClient("Result.xlsx");
+
+            await blob.UploadAsync(uploadstream);
+        }
+
+        public async void UploadPdfAsync(MemoryStream uploadstream)
+        {
+            BlobContainerClient container = new BlobContainerClient(storageConnectionString, "diodocs");
+
+            BlobClient blob = container.GetBlobClient("Result.pdf");
+
+            await blob.UploadAsync(uploadstream);
+        }
+    }
+}
